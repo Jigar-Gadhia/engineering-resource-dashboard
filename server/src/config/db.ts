397a5d9917b550bc0c1c89f78156d7cc5fd3c2eb
unknown <jiggsgadhia@gmail.com>
2025-10-29 12:33:27 +0530

    fixed mobile sidebar issue

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err);
     process.exit(1);
